0c4eaad04e42db156c41ee86a82c6b54f840e9eb
gadorlhiac <gadorlhiac@gmail.com>
2025-03-18 08:03:37 -0700

    Update jidtasks to use Failed/Completed

--- tasks/jidtasks.py
+++ tasks/jidtasks.py
@@ -14,7 +14,6 @@ Classes:
 __all__ = ["run_managed_task"]
 __author__ = "Gabriel Dorlhiac"
 
-import sys
 import uuid
 import time
 import logging
@@ -23,6 +22,7 @@ from typing import Dict, Any, Union, List, Optional
 
 import requests
 from prefect import task
+from prefect.states import Completed, Failed, State
 
 from flow_dataclasses import FlowConf, LuteParams
 
@@ -36,6 +36,13 @@ JID_API_ENDPOINTS: Dict[str, str] = {
     "job_statuses": "job_statuses",
     "job_log_file": "{experiment}/job_log_file",
 }
+
+
+class JIDException(Exception): ...
+
+
+class MissingParametersException(Exception): ...
+
 
 def sub_overridable_arguments(
     slurm_param_str: str,
@@ -54,6 +61,16 @@ def sub_overridable_arguments(
     Args:
         slurm_param_str (str): Constructed string of DAG SLURM arguments
             without modification
+
+        max_cores (Optional[int]): Optionally override number of cores for SLURM
+            job submission.
+
+        max_nodes (Optional[int]): Optionally override number of nodes for SLURM
+            job submission.
+
+        require_partition (Optional[str]): Optionally require a specific job by run
+            on a specific SLURM partition.
+
     Returns:
         slurm_param_str (str): Modified SLURM argument string.
     """
@@ -65,9 +82,7 @@ def sub_overridable_arguments(
         try:
             ntasks = int(re.findall(pattern, slurm_param_str)[0])
             if ntasks > max_cores:
-                slurm_param_str = re.sub(
-                    pattern, f"{max_cores}", slurm_param_str
-                )
+                slurm_param_str = re.sub(pattern, f"{max_cores}", slurm_param_str)
         except IndexError:  # If `ntasks` not passed - 1 is default
             ntasks = 1
             slurm_param_str = f"{slurm_param_str} --ntasks={ntasks}"
@@ -79,9 +94,7 @@ def sub_overridable_arguments(
             _ = re.findall(pattern, slurm_param_str)[0]
             # Check if present with above. Below does nothing but does not
             # throw error if pattern not present.
-            slurm_param_str = re.sub(
-                pattern, f"nodes=0-{max_nodes}", slurm_param_str
-            )
+            slurm_param_str = re.sub(pattern, f"nodes=0-{max_nodes}", slurm_param_str)
         except IndexError:  # `--nodes` not present
             slurm_param_str = f"{slurm_param_str} --nodes=0-{max_nodes}"
 
@@ -96,11 +109,10 @@ def sub_overridable_arguments(
                 pattern, f"partition={require_partition}", slurm_param_str
             )
         except IndexError:  # --partition not present. This shouldn't happen
-            slurm_param_str = (
-                f"{slurm_param_str} --partition={require_partition}"
-            )
+            slurm_param_str = f"{slurm_param_str} --partition={require_partition}"
 
     return slurm_param_str
+
 
 def create_control_doc(
     lute_task_id: str,
@@ -116,9 +128,21 @@ def create_control_doc(
     JID.
 
     Args:
-        context (Dict[str, Any]): Airflow dictionary object.
-            https://airflow.apache.org/docs/apache-airflow/stable/templates-ref.html
-            contains a list of available variables and their description.
+        lute_task_id (str): Name of the LUTE Managed Task to run.
+
+        conf (FlowConf): Dictionary containing information for LUTE submission.
+
+        custom_slurm_params (str): Optionally override all the SLURM options which are
+            present in the `conf` object.
+
+        max_cores (Optional[int]): Optionally override number of cores for SLURM
+            job submission.
+
+        max_nodes (Optional[int]): Optionally override number of nodes for SLURM
+            job submission.
+
+        require_partition (Optional[str]): Optionally require a specific job by run
+            on a specific SLURM partition.
 
     Returns:
         control_doc (Dict[str, Union[str, Dict[str, str]]]): JID job control
@@ -132,16 +156,14 @@ def create_control_doc(
 
     if lute_params == {}:
         logger.critical("Empty LUTE parameter dictionary! Need configuration YAML!")
-        sys.exit(-1)
+        raise MissingParametersException
     config_path: str = lute_params["config_file"]
     # Note that task_id is from the parent class.
     # When defining the Operator instances the id is assumed to match a
     # managed task!
     lute_param_str: str
     if lute_params["debug"]:
-        lute_param_str = (
-            f"--taskname {lute_task_id} --config {config_path} --debug"
-        )
+        lute_param_str = f"--taskname {lute_task_id} --config {config_path} --debug"
     else:
         lute_param_str = f"--taskname {lute_task_id} --config {config_path}"
 
@@ -157,7 +179,9 @@ def create_control_doc(
         slurm_param_str = " ".join(conf.get("slurm_params"))
 
         # Make any requested SLURM argument substitutions
-        slurm_param_str = sub_overridable_arguments(slurm_param_str, max_cores, max_nodes, require_partition)
+        slurm_param_str = sub_overridable_arguments(
+            slurm_param_str, max_cores, max_nodes, require_partition
+        )
 
     parameter_str: str = f"{lute_param_str} {slurm_param_str}"
     jid_job_definition: Dict[str, str] = {
@@ -184,6 +208,7 @@ def create_control_doc(
 
     return control_doc
 
+
 def parse_response(
     resp: requests.models.Response, check_for_error: List[str]
 ) -> Dict[str, Any]:
@@ -192,19 +217,16 @@ def parse_response(
     Args:
         resp (requests.models.Response): The response object from a JID
             HTTP request.
+
         check_for_error (List[str]): A list of strings/patterns to search
             for in response. Exception is raised if there are any matches.
 
     Returns:
         value (Dict[str, Any]): Dictionary containing HTTP response value.
-
-    Raises:
-        AirflowException: Raised to translate multiple errors into object
-            properly handled by the Airflow server.
     """
     logger.debug(f"{resp.status_code}: {resp.content}")
     if resp.status_code not in (200,):
-        raise Exception(f"Bad response from JID {resp}: {resp.content}")
+        raise JIDException(f"Bad response from JID {resp}: {resp.content}")
     try:
         json: Dict[str, Union[str, int]] = resp.json()
         if json.get("success", "") not in (True,):
@@ -218,9 +240,8 @@ def parse_response(
                 )
         return value
     except Exception as err:
-        raise Exception(
-            f"Response from JID not parseable, unknown error: {err}"
-        )
+        raise JIDException(f"Response from JID not parseable, unknown error: {err}")
+
 
 def rpc(
     endpoint: str,
@@ -241,9 +262,7 @@ def rpc(
         control_doc (Dict[str, Union[str, Dict[str, str]]]): Dictionary for
             JID call.
 
-        context (Dict[str, Any]): Airflow dictionary object.
-            https://airflow.apache.org/docs/apache-airflow/stable/templates-ref.html
-            contains a list of available variables and their description.
+        conf (FlowConf): Dictionary containing information for LUTE submission.
 
         check_for_error (List[str]): A list of keywords to search for in a
             response to indicate error conditions. Default [].
@@ -271,6 +290,7 @@ def rpc(
 
     return value
 
+
 @task
 def run_managed_task(
     lute_task_id: str,
@@ -280,52 +300,72 @@ def run_managed_task(
     max_nodes: Optional[int] = None,
     require_partition: Optional[str] = None,
     custom_slurm_params: str = "",
-) -> str:
-    """Method called by Airflow which submits SLURM Job via JID.
+) -> State:
+    """Task which submits a LUTE Managed Task via the JID.
 
     Args:
-        context (Dict[str, Any]): Airflow dictionary object.
-            https://airflow.apache.org/docs/apache-airflow/stable/templates-ref.html
-            contains a list of available variables and their description.
+        lute_task_id (str): Name of the LUTE Managed Task to run.
+
+        conf (FlowConf): Dictionary containing information for LUTE submission.
+
+        poke_interval (float): Status polling interval in seconds.
+
+        max_cores (Optional[int]): Optionally override number of cores for SLURM
+            job submission.
+
+        max_nodes (Optional[int]): Optionally override number of nodes for SLURM
+            job submission.
+
+        require_partition (Optional[str]): Optionally require a specific job by run
+            on a specific SLURM partition.
+
+        custom_slurm_params (str): Optionally override all the SLURM options which are
+            present in the `conf` object.
     """
-    # logger.info(f"Attempting to run at {self.get_location(context)}...")
     logger.info("Attempting to run at S3DF.")
-    control_doc = create_control_doc(
-        lute_task_id=lute_task_id,
-        conf=conf,
-        custom_slurm_params=custom_slurm_params,
-        max_cores=max_cores,
-        max_nodes=max_nodes,
-        require_partition=require_partition,
-    )
+    try:
+        control_doc = create_control_doc(
+            lute_task_id=lute_task_id,
+            conf=conf,
+            custom_slurm_params=custom_slurm_params,
+            max_cores=max_cores,
+            max_nodes=max_nodes,
+            require_partition=require_partition,
+        )
+    except MissingParametersException as err:
+        return Failed(message=f"Missing conf parameters: {err}")
+
     logger.info(control_doc)
     logger.info(f"{JID_API_LOCATION}/{JID_API_ENDPOINTS['start_job']}")
     # start_job requires a dictionary
-    msg: Dict[str, Any] = rpc(
-        endpoint="start_job", control_doc=control_doc, conf=conf
-    )
-    logger.info(f"JobID {msg['tool_id']} successfully submitted!")
-
-    jobs: List[Dict[str, Any]] = [msg]
-    logger.info("Checking for job completion.")
-    while jobs[0].get("status") in ("RUNNING", "SUBMITTED"):
-        jobs = rpc(
-            endpoint="job_statuses",
-            control_doc=jobs,
-            conf=conf,
-            check_for_error=[" error: ", "Traceback"]
+    try:
+        msg: Dict[str, Any] = rpc(
+            endpoint="start_job", control_doc=control_doc, conf=conf
         )
-        time.sleep(poke_interval)
+        logger.info(f"JobID {msg['tool_id']} successfully submitted!")
 
-    # Logs out to xcom
-    out = rpc("job_log_file", jobs[0], conf)
-    #context["task_instance"].xcom_push(key="log", value=out)
-    final_status: str = jobs[0].get("status")
-    logger.info(f"Final status: {final_status}")
-    if final_status in ("FAILED", "EXITED"):
-        # Only DONE indicates success. EXITED may be cancelled or SLURM err
-        logger.error(f"`Task` job marked as {final_status}!")
-        sys.exit(-1)
+        jobs: List[Dict[str, Any]] = [msg]
+        logger.info("Checking for job completion.")
+        while jobs[0].get("status") in ("RUNNING", "SUBMITTED"):
+            jobs = rpc(
+                endpoint="job_statuses",
+                control_doc=jobs,
+                conf=conf,
+                check_for_error=[" error: ", "Traceback"],
+            )
+            time.sleep(poke_interval)
+
+        # Grab Task logs
+        out = rpc("job_log_file", jobs[0], conf)
+        # context["task_instance"].xcom_push(key="log", value=out)
+        final_status: str = jobs[0].get("status")
+        logger.info(f"Final status: {final_status}")
+        if final_status in ("FAILED", "EXITED"):
+            # Only DONE indicates success. EXITED may be cancelled or SLURM err
+            logger.error(f"`Task` job marked as {final_status}!")
+            return Failed(message=f"`Task` job marked as {final_status}")
+    except JIDException as err:
+        return Failed(message=f"JIDException: {err}")
 
     failure_messages: List[str] = [
         "INFO:lute.execution.executor:Task failed with return code:",
@@ -335,7 +375,7 @@ def run_managed_task(
     for failure_msg in failure_messages:
         if failure_msg in out:
             logger.error("Logs indicate `Task` failed!")
-            sys.exit(-1)
+            return Failed(message="Logs indicate `Task` failed!")
 
     print(out)
-    return out
+    return Completed(message="Task completed successfully.")
